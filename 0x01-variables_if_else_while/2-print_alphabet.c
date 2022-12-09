7cc27b04aed09df30a5df2ef309cf26a1b236288
George Nyarko <genyarko89@gmail.com>
2022-12-09 08:31:14 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
-
+#include <stdio.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- *main - Entry point
- *
- *Return: Always 0 (success)
-
- */
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
 
 int main(void)
 {
