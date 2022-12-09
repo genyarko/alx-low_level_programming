047d5c180ff514831bba31b6b23d06b572ff7962
George Nyarko <genyarko89@gmail.com>
2022-12-09 00:28:24 -0800

    first again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,50 +12,38 @@
 
 /**
 
- *main - Entry point
+*main - Entry point
 
- *
+*
 
- *Return: Always 0 (success)
+*Return: Always 0 (success)
 
- */
+*/
 
 int main(void)
-  
-{
-  
-  int n;
-  
 
-  
-  srand(time(0));
-  
-  n = rand() - RAND_MAX / 2;
-  
-  if(n>0)
-    
-    {
-      
-      printf("%d is positive", n);
-      
-    }
-  
-  else if(n==0)
-    
-    {
-      
-      printf("%d is zero", n);
-      
-    }
-  
-  else
-    
-    {
-      
-      printf("%d is negative", n);
-      
-    }
-  
-  return (0);
-  
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+
+	{printf("%d is positive", n);}
+
+	else if (n == 0)
+
+	{printf("%d is zero", n);}
+
+	else 
+
+	{printf("%d is negative", n);}
+
+	return (0);
+
 }
