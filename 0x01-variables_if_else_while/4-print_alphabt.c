f28ee1cf7cc469d0fba1c47b2623368d6ffa0d4a
George Nyarko <genyarko89@gmail.com>
2022-12-09 03:12:24 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+
+#include <unistd.h>
+
+
+
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+if (c != 'q' && c != 'e')
+{
+putchar(c);
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
