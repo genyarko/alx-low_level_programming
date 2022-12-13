19b8c3952f3725be01a1d125c9787c82923b93e9
George Nyarko <genyarko89@gmail.com>
2022-12-13 02:56:47 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+/**
+* print_last_digit - prints the last digit of a number
+* @n: number to be evaluated
+* Return: last digit of a number
+*/
+int print_last_digit(int n)
+{
+int last_digit;
+last_digit = n % 10;
+if (last_digit < 0)
+{
+last_digit = last_digit * -1;
+}
+putchar(last_digit + '0');
+return (last_digit);
+}
