9d38a70eb15c0d4ff98ca8cb0848dd509ee6cf29
George Nyarko <genyarko89@gmail.com>
2022-12-13 03:11:10 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+voidft_putchar(char c)
+{
+write(1, &c, 1);
+}
+voidft_putnbr(int nb)
+{
+if (nb < 0)
+{
+ft_putchar('-');
+nb = -nb;
+}
+if (nb >= 10)
+{
+ft_putnbr(nb / 10);
+ft_putnbr(nb % 10);
+}
+else
+ft_putchar(nb + '0');
+}
+voidjack_bauer(void)
+{
+inti;
+intj;
+i = 0;
+while (i < 24)
+{
+j = 0;
+while (j < 60)
+{
+ft_putnbr(i);
+ft_putchar(':');
+ft_putnbr(j);
+ft_putchar('\n');
+j++;
+}
+i++;
+}
+}
