77d81c0c9b5f47e113445f40a7b92107c91b0ea6
George Nyarko <genyarko89@gmail.com>
2022-12-13 12:24:32 -0800

    bs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,12 @@
 */
 int print_last_digit(int n)
 {
-int last_digit= n % 10;
+int last_digit;
+last_digit  = n % 10;
 if (last_digit < 0)
+{
 last_digit = last_digit * -1;
+}
 putchar(last_digit + '0');
-return (0);
+return (last_digit);
 }
