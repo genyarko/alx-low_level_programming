ceff20f265fc145100a629826265828f7199601e
George Nyarko <genyarko89@gmail.com>
2022-12-13 13:22:00 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-  int arr[11] = {2, 8, 34, 144, 610, 2584, 10946, 46368, 196418, 832040, 3524578};
+int ar[11] = {2, 8, 34, 144, 610, 2584, 10946, 46368, 196418, 832040, 3524578};
 int sum = 0;
 int i;
 for (i = 0; i < 11; i++)
 {
-sum += arr[i];
+sum += ar[i];
 }
 printf("Sum of array is %d\n", sum);
 return (0);
