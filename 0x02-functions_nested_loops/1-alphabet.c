3e529c73b625dcf7bb3deb51300fad6ec3a3c1d1
George Nyarko <genyarko89@gmail.com>
2022-12-13 11:09:20 -0800

    kidding

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
-* main - check the code
-* print_alphabet
-* Return: Always 0.
+*print_alphabet - entry point
+* Return: 0.
 */
 void print_alphabet(void)
 {
-/**
-*print_alphabet used
-*/
+
 int i;
 for (i = 'a'; i <= 'z'; i++)
 {
