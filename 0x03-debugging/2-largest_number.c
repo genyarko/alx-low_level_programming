575d76fda766975c66a090695a81719e771566ef
George Nyarko <genyarko89@gmail.com>
2022-12-14 13:32:41 -0800

    shitty checker

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,38 +11,9 @@ int largest_number(int a, int b, int c)
 int largest;
 if (a >= b && b >= c)
 largest = a;
-else if (b >= a && a >= c)
+if (b >= a && a >= c)
 largest = b;
-else if (c >= a && c >= b)
+if (c >= a && c >= b)
 largest = c;
 return (largest);
-}
-int main(void)
-  
-{
-  
-  int a, b, c;
-  
-  int largest;
-  
-
-  
-  a = 972;
-  
-  b = -98;
-  
-  c = 0;
-  
-
-  
-  largest = largest_number(a, b, c);
-  
-
-  
-  printf("%d is the largest number\n", largest);
-  
-
-  
-  return (0);
-  
 }
