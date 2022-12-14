9b7b629547f643e320612904f80fe3e5d553f0d3
George Nyarko <genyarko89@gmail.com>
2022-12-14 03:50:10 -0800

    dd

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -32,3 +32,32 @@ largest = b;
 }
 return (largest);
 }
+int main(void)
+  
+{
+  
+  int a, b, c;
+  
+  int largest;
+  
+
+  
+  a = 972;
+  
+  b = -98;
+  
+  c = 0;
+  
+
+  
+  largest = largest_number(a, b, c);
+  
+
+  
+  printf("%d is the largest number\n", largest);
+  
+
+  
+  return (0);
+  
+}
