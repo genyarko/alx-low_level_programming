aca7a38325b053f0bfcebee9b37fd73145be0d8f
George Nyarko <genyarko89@gmail.com>
2022-12-14 03:20:09 -0800

    easier

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - causes an infinite loop
+* Return: 0
+* \\o/\n - dont ask me
+* while (i<10) - trying to escape an infinite loop
+*/
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/**
+* while (i < 10)
+* {
+* putchar(i);
+* }
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,20 @@
+carrie@ubuntu:/debugging$ cat main.h
+
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void printing_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_tale(int n);
+
+
+#endif /* MAIN_H */
