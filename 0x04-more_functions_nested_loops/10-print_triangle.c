d52fa7de2e90a1459333768f602007e597598c6c
George Nyarko <genyarko89@gmail.com>
2022-12-15 02:23:33 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+/**
+* print_triangle - prints a triangle
+* @size: size of triangle
+* Return: void
+*/
+
+void print_triangle(int size)
+{
+int i, j, k;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+k = size - i - 1;
+if (j < k)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+}
