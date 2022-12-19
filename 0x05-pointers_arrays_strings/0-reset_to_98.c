7b22bae4d995975b818e952d2fc2c5d50d37ede8
George Nyarko <genyarko89@gmail.com>
2022-12-19 01:37:36 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+* update_int - updates the value of the int pointer to 98
+* @pointer_int: a pointer to an int
+*
+* Return: void
+*/
+void update_int (int *pointer_int) {
+*pointer_int = 98;
+}
