e99c3f5c45a14e97699ebbf80aa0d050eb8f396c
George Nyarko <genyarko89@gmail.com>
2022-12-21 12:42:26 -0800

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -147,3 +147,31 @@ else
 putchar('0');
 }
 }
+
+int main(void)
+  
+{
+  
+  print_number(98);
+  
+  putchar('\n');
+  
+  print_number(402);
+  
+  putchar('\n');
+  
+  print_number(1024);
+  
+  putchar('\n');
+  
+  print_number(0);
+  
+  putchar('\n');
+  
+  print_number(-98);
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
