42356c887ba23267c06dcfb78a8710c119a0d67a
George Nyarko <genyarko89@gmail.com>
2022-12-22 18:34:52 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,131 +1,115 @@
 #include <stdio.h>
-
 /**
+ * print_number - prints an integer
+ * @n: integer to print
  *
- *  * print_number - prints an integer
- *
- *   * @n: integer to print
- *
- *    *
- *
- *     * Return: void
- *
- *      */
+ * Return: void
+ */
 
 void print_number(int n)
+{
+int i, j, k, l, m, p;
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+i = n / 1000000000;
+
+j = (n / 100000000) % 10;
+
+k = (n / 10000000) % 10;
+
+l = (n / 1000000) % 10;
+
+m = (n / 100000) % 10;
+
+p = (n / 10000) % 10;
+
+if (i != 0)
 
 {
 
-		int i, j, k, l, m, p;
+putchar(i + '0');
 
+putchar(j + '0');
 
+putchar(k + '0');
 
-			if (n < 0)
+putchar(l + '0');
 
-					{
+putchar(m + '0');
 
-								putchar('-');
+putchar(p + '0');
 
-										n = -n;
+}
 
-											}
+else if (j != 0)
 
-				i = n / 1000000000;
+{
 
-					j = (n / 100000000) % 10;
+putchar(j + '0');
 
-						k = (n / 10000000) % 10;
+putchar(k + '0');
 
-							l = (n / 1000000) % 10;
+putchar(l + '0');
 
-								m = (n / 100000) % 10;
+putchar(m + '0');
 
-									p = (n / 10000) % 10;
+putchar(p + '0');
 
-										if (i != 0)
+}
 
-												{
+else if (k != 0)
 
-															putchar(i + '0');
+{
 
-																	putchar(j + '0');
+putchar(k + '0');
 
-																			putchar(k + '0');
+putchar(l + '0');
 
-																					putchar(l + '0');
+putchar(m + '0');
 
-																							putchar(m + '0');
+putchar(p + '0');
 
-																									putchar(p + '0');
+}
 
-																										}
+else if (l != 0)
 
-											else if (j != 0)
+{
 
-													{
+putchar(l + '0');
 
-																putchar(j + '0');
+putchar(m + '0');
 
-																		putchar(k + '0');
+putchar(p + '0');
 
-																				putchar(l + '0');
+}
 
-																						putchar(m + '0');
+else if (m != 0)
 
-																								putchar(p + '0');
+{
 
-																									}
+putchar(m + '0');
 
-												else if (k != 0)
+putchar(p + '0');
 
-														{
+}
 
-																	putchar(k + '0');
+else if (p != 0)
 
-																			putchar(l + '0');
+{
 
-																					putchar(m + '0');
+putchar(p + '0');
 
-																							putchar(p + '0');
+}
 
-																								}
+putchar((n / 1000) % 10 + '0');
 
-													else if (l != 0)
+putchar((n / 100) % 10 + '0');
 
-															{
+putchar((n / 10) % 10 + '0');
 
-																		putchar(l + '0');
-
-																				putchar(m + '0');
-
-																						putchar(p + '0');
-
-																							}
-
-														else if (m != 0)
-
-																{
-
-																			putchar(m + '0');
-
-																					putchar(p + '0');
-
-																						}
-
-															else if (p != 0)
-
-																	{
-
-																				putchar(p + '0');
-
-																					}
-
-																putchar((n / 1000) % 10 + '0');
-
-																	putchar((n / 100) % 10 + '0');
-
-																		putchar((n / 10) % 10 + '0');
-
-																			putchar(n % 10 + '0');
+putchar(n % 10 + '0');
 
 }
