0eafba27d600491db0de5315b7a4e3e8dded4311
George Nyarko <genyarko89@gmail.com>
2023-01-05 02:19:26 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,87 @@
+/**
+
+ * is_palindrome - checks if a string is a palindrome
+
+ * @s: string to check
+
+ *
+
+ * Return: 1 if palindrome, 0 if not
+
+ */
+
+int is_palindrome(char *s)
+  
+{
+  
+  int len = _strlen_recursion(s);
+  
+
+  
+  if (len <= 1)
+    
+    return (1);
+  
+  return (check_palindrome(s, len));
+  
+}
+
+
+
+/**
+
+ * check_palindrome - checks if a string is a palindrome
+
+ * @s: string to check
+
+ * @len: length of string
+
+ *
+
+ * Return: 1 if palindrome, 0 if not
+
+ */
+
+int check_palindrome(char *s, int len)
+  
+{
+  
+  if (len <= 1)
+    
+    return (1);
+  
+  else if (*s == *(s + len - 1))
+    
+    return (check_palindrome(s + 1, len - 2));
+  
+  else
+    
+    return (0);
+  
+}
+
+
+
+/**
+
+ * _strlen_recursion - returns the length of a string
+
+ * @s: string to check
+
+ *
+
+ * Return: length of string
+
+ */
+
+int _strlen_recursion(char *s)
+  
+{
+  
+  if (*s == '\0')
+    
+    return (0);
+  
+  return (1 + _strlen_recursion(s + 1));
+  
+}
