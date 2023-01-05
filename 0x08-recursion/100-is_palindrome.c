852bc49d664bcf5e5048fc20ecb6fe65b7be6f31
George Nyarko <genyarko89@gmail.com>
2023-01-05 02:34:27 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,83 +1,44 @@
 /**
-
- * _strlen_recursion - returns the length of a string
-
- * @s: string to check
-
- *
-
- * Return: length of string
-
- */
+* _strlen_recursion - returns the length of a string
+* @s: string to check
+*
+* Return: length of string
+*/
 
 int _strlen_recursion(char *s)
-
 {
-
-	if (*s == '\0')
-
-		return (0);
-
-	else
-
-		return (1 + _strlen_recursion(s + 1));
-
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
 /**
-
- * check_palindrome - checks if a string is a palindrome
-
- * @s: string to check
-
- * @len: length of string
-
- *
-
- * Return: 1 if palindrome, 0 if not
-
- */
+* check_palindrome - checks if a string is a palindrome
+* @s: string to check
+* @len: length of string
+*
+* Return: 1 if palindrome, 0 if not
+*/
 
 int check_palindrome(char *s, int len)
-
 {
-
-	if (len <= 1)
-
-		return (1);
-
-	else if (*s == *(s + len - 1))
-
-		return (check_palindrome(s + 1, len - 2));
-
-	else
-
-		return (0);
-
+if (len <= 1)
+return (1);
+else if (*s == *(s + len - 1))
+return (check_palindrome(s + 1, len - 2));
+else
+return (0);
 }
 /**
-
- * is_palindrome - checks if a string is a palindrome
-
- * @s: string to check
-
- *
-
- * Return: 1 if palindrome, 0 if not
-
- */
-
+* is_palindrome - checks if a string is a palindrome
+* @s: string to check
+*
+* Return: 1 if palindrome, 0 if not
+*/
 int is_palindrome(char *s)
-  
 {
-  
-  int len = _strlen_recursion(s);
-  
-
-  
-  if (len <= 1)
-    
-    return (1);
-  
-  return (check_palindrome(s, len));
-  
+int len = _strlen_recursion(s);
+if (len <= 1)
+return (1);
+return (check_palindrome(s, len));
 }
