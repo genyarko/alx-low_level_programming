b030b8509f395e2376a9b34c7d5239772dbe6115
George Nyarko <genyarko89@gmail.com>
2023-01-09 12:18:47 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,30 +1,12 @@
-/**                                                                                                                                  
+#include <unistd.h>
+/**                                                                            * main - print the name of the program                                        
+* @argc: Count arguments                                                       * @argv: Arguments                                                             * Return: Always 0 (Success)                                                   
+*/
 
- * main - print the name of the program                                                                                              
-
- * @argc: Count arguments                                                                                                            
-
- * @argv: Arguments                                                                                                                  
-
- *                                                                                                                                   
-
- * Return: Always 0 (Success)                                                                                                        
-
- */                                                                                                                                  
-
-                                                                                                                                     
-
-int main(int argc, char *argv[])                                                                                                     
-
-{                                                                                                                                    
-
-(void) argv; /*Ignore argv*/                                                                                                         
-
-printf("%i\n", argc - 1);                                                                                                            
-
-                                                                                                                                     
-
-return (0);                                                                                                                          
-
+int main(int argc, char **argv)
+{
+(void)argv;
+write(1, &argc, 1);
+write(1, "\n", 1);
+return (0);
 }
-
