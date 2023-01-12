3f27744c06487fe68a6e2266d81a62dd2ace96a7
George Nyarko <genyarko89@gmail.com>
2023-01-12 08:27:46 -0800

    betty

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,71 +1,34 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
-
-
 /**
-
- * main - multiplies two positive numbers
-
- * @argc: argument count
-
- * @argv: argument vector
-
- *
-
- * Return: 0 on success, 1 on error
-
- */
-
+* main - multiplies two positive numbers
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0 on success, 1 on error
+*/
 int main(int argc, char *argv[])
-
 {
-
-	int i, j;
-
-	unsigned long int mul = 1;
-
-
-
-	if (argc != 3)
-
-	{
-
-		printf("Error\n");
-
-		exit(98);
-
-	}
-
-	for (i = 1; i < argc; i++)
-
-	{
-
-		for (j = 0; argv[i][j]; j++)
-
-		{
-
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-
-			{
-
-				printf("Error\n");
-
-				exit(98);
-
-			}
-
-		}
-
-		mul *= atoi(argv[i]);
-
-	}
-
-	printf("%lu\n", mul);
-
-	return (0);
-
+int i, j;
+unsigned long int mul = 1;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+exit(98);
+}
+}
+mul *= atoi(argv[i]);
+}
+printf("%lu\n", mul);
+return (0);
 }
