9dd51ffcc9ff444aecd6b8d6e68313d2769192c3
genyarko <115112347+genyarko@users.noreply.github.com>
2023-02-02 21:00:53 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,46 @@
+#include "main.h"
 /**
-* print_binary - prints the binary representation of a number
-* @n: number to convert
-*
-* Description: This function uses bitwise shifts to print the binary 
-* representation of a number. It does not use arrays, malloc, %, or /.
-*/
+ * _power - calculate (base and power)
+ * @base: base of the exponet
+ * @pow: power of the exponet
+ * Return: value of base and power
+ */
+unsigned long int _power(unsigned int base, unsigned int pow)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= pow; i++)
+		num *= base;
+	return (num);
+}
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: num of prented
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-unsigned long int binary;
-int i;
-if (n == 0)
-printf("0");
-for (i = ((sizeof(n) * 8) - 1); i >= 0; i--)
-{
-binary = n >> i;
-if (binary & 1)
-printf("1");
-else
-printf("0");
-}
-printf("\n");
+	unsigned long int dev, result;
+	char flag;
+
+	flag = 0;
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (dev != 0)
+	{
+		result = n & dev;
+		if (result == dev)
+		{
+			flag = 1;
+			_putchar('1');
+
+		}
+		else if (flag == 1 || dev == 1)
+		{
+			_putchar('0');
+		}
+		dev >>= 1;
+	}
 }
