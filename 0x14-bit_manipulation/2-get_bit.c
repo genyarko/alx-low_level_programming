101d567a226a8df3cc087eb0057922439c171992
genyarko <115112347+genyarko@users.noreply.github.com>
2023-02-02 20:56:27 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,25 @@
-#include <stdio.h>
-#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if there is one or more chars in the
+ * string b that is not 0 or 1, or b is NULL
+ */
 unsigned int binary_to_uint(const char *b)
 {
-int i = 0;
-unsigned int result = 0;
-if (b == NULL)
-return (0);
-while (b[i] != '\0')
-{
-if (b[i] != '1' && b[i] != '0')
-return (0);
-if (b[i] == '1')
-result = (result << 1) + 1;
-else
-result = result << 1;
-i++;
-}
-return (result);
+	unsigned int num = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b == '1')
+			num = (num << 1) | 1;
+		else if (*b == '0')
+			num = num << 1;
+		else
+			return (0);
+		b++;
+	}
+	return (num);
 }
