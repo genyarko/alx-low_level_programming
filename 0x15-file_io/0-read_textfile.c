21f8f1871072d9c5be327b52549a0dfb0edc9cf5
genyarko <115112347+genyarko@users.noreply.github.com>
2023-02-06 20:47:36 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
-/*
-* create_file - Creates a file with the given name and content.
-* @filename: Name of the file to create.
-* @text_content: NULL terminated string to write to the file.
-*
-* Description: Creates a file with the given name and content.
-* The created file must have the permissions rw-------. If the file
-* already exists, it is truncated.
-*
-* Return: 1 on success, -1 on failure (file can not be created,
-* file can not be written, write “fails”, etc...)
-*/
-int create_file(const char *filename, char *text_content)
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    int ret;
-    mode_t mode = S_IRUSR | S_IWUSR;
+	ssize_t o, r, w;
+	char *buffer;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, mode);
-    if (fd == -1)
-        return (-1);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    if (text_content)
-    {
-        ret = write(fd, text_content, strlen(text_content));
-        if (ret == -1)
-            return (-1);
-    }
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-    close(fd);
-    return (1);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
