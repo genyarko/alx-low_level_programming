82a451f1dec685a22b300760c487f2ee416eee2a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-02-06 20:54:07 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,48 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ *  read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ *  @filename: name of the file to be read
+ *  @letters: number of letters it should read and print
+ *
+ *  Return: the actual number of letters it could read and print
+ *  if the file can not be opened or read, return 0
+ *  if filename is NULL return 0
+ *  if write fails or does not write the expected amount of bytes, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	int fd;
+	ssize_t n_bytes;
+	char *buffer;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
-
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
+	n_bytes = read(fd, buffer, letters);
+	if (n_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	n_bytes = write(STDOUT_FILENO, buffer, n_bytes);
+	if (n_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (n_bytes);
 }
