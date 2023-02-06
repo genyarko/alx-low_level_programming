7aed78dcbfc8d5f6abf6e8feb4dc2721df5f380b
genyarko <115112347+genyarko@users.noreply.github.com>
2023-02-06 20:23:40 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,32 @@
-/*
- * File: 0-read_textfile.c
- * Auth: Dr Marcus.
- */
-#include "main.h"
-#include <stdlib.h>
-
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - reads a text file and prints it to POSIX standard output
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * @filename: pointer to file name
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print, 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd;
+	ssize_t len = 0;
+	char *buf = NULL;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
-	}
-
-	free(buffer);
-	close(o);
-
-	return (w);
+	len = read(fd, buf, letters);
+	if (len < 0)
+		return (0);
+	len = write(STDOUT_FILENO, buf, len);
+	if (len < 0 || len != (ssize_t)letters)
+		return (0);
+	free(buf);
+	close(fd);
+	return (len);
 }
