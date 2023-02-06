7188b1a78a876a66517df3718f6f59e3c9fb295c
genyarko <115112347+genyarko@users.noreply.github.com>
2023-02-06 20:31:35 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,36 @@
 #include "main.h"
-/**
- * create_file -creates an array of chars, and initializes
+/*
+ * create_file - Creates a file with the given name and content.
+ * @filename: Name of the file to create.
+ * @text_content: NULL terminated string to write to the file.
  *
- * @text_content: is a NULL terminated string to write to the file
- * @filename: is the name of the file to create
+ * Description: Creates a file with the given name and content.
+ * The created file must have the permissions rw-------. If the file
+ * already exists, it is truncated.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc...)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+    int fd;
+    int ret;
+    mode_t mode = S_IRUSR | S_IWUSR;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+    fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, mode);
+    if (fd == -1)
+        return (-1);
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+    if (text_content)
+    {
+        ret = write(fd, text_content, strlen(text_content));
+        if (ret == -1)
+            return (-1);
+    }
 
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
-	return (1);
+    close(fd);
+    return (1);
 }
