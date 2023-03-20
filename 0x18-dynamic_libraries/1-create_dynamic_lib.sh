ea875e36f7b423bbd54ed15ab784b681772e1547
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-20 20:46:41 +0100

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,24 +1,2 @@
-#!/bin/bash
-# Create liball.so from all .c files in current directory
-
-# Check for .c files in directory
-if [ -z $(ls *.c) ]; then
-    echo "No .c files found in directory."
-    exit
-fi
-
-# Create .o files from .c files
-for cfile in *.c; do
-    gcc -c -Wall -Werror -fpic $cfile
-done
 gcc *.c -c -fPIC
 gcc *.o -shared -o liball.so
-
-
-# Create liball.so from .o files
-gcc -shared -o liball.so *.o
-
-# Cleanup .o files
-rm *.o
-
-echo "liball.so created successfully!"
