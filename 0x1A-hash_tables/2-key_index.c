2ac51f94165bbdbfa771864ae2db8b3cbc4d1230
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-08 06:13:52 +0200

    Create 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+/**
+ * key_index - compute the index of a key in the hash table
+ * @key: the key to hash
+ * @size: the size of the hash table's array
+ *
+ * Return: the index at which the key/value pair should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = hash_djb2(key);
+	unsigned long int index = hash % size;
+	return index;
+}
